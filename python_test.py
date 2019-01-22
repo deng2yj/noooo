a798f05d416c4e8ad5da9a77b504dbc0266d4896
邓瑜jie <DYJ@201.208.21.198.tigernet.wifi.dyn.clemson.edu>
2019-01-22 16:20:39 -0500

    testing python numbers

---
+++ python_test.py
@@ -0,0 +1,41 @@
+print("hello")
+
+print(19/3)
+
+print(19//3)
+
+print(19%3)
+
+print(18/3)
+
+print(5**2)
+
+print(2**10)
+
+a = 10
+b = 20
+print(a+b)
+
+
+print("with variable")
+a = 19
+print(a/3)
+
+print(a//3)
+
+print(a%3)
+
+print("\n")
+#print(c+1)
+print("hello world")
+
+def addandmultiply(a,b):
+	print(a+b)
+	print(a*b)
+
+
+addandmultiply(10,20)
+addandmultiply(2,19)
+addandmultiply(102,339)
+
+print(round(1.22322342342342323423,3))
