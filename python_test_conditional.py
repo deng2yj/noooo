5e06ceb92d0de92cb99ab658a468c61860dbdb88
邓瑜jie <DYJ@32.213.21.198.tigernet.wifi.dyn.clemson.edu>
2019-01-24 15:44:52 -0500

    discussion about range

---
+++ python_test_conditional.py
@@ -0,0 +1,57 @@
+x = 5
+
+if x < 0:
+	print("x is smaller than 0.")
+elif x > 10:
+	print("x is bigger than 10.")
+else:
+	print("x is between 0 and 10")
+
+########
+#y=int(input("enter value of y:"))
+y=10
+if y<0:
+	print("y is small.")
+elif y >= 0:
+	print("y is big.")
+else:
+	print("error")
+
+##########
+
+animals = ['cat','dog','turtle','bird']
+
+print(animals)
+
+for x in animals:
+	print(x)
+	print(len(x))
+
+#numbers = [1,2,3,4,5,6,7,8,9,10]
+
+numbers = range(1,11)
+for i in numbers:
+	print(i**2)
+
+
+
+def complicated_function():
+	pass
+
+##in R
+#  complicated_function <- function(){
+	
+# }
+
+
+
+
+
+
+
+
+
+
+
+
+
