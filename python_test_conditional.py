bb50d0e60698f9541608eee0489e8423e4e234c2
邓瑜jie <DYJ@32.213.21.198.tigernet.wifi.dyn.clemson.edu>
2019-01-24 16:41:52 -0500

    whileloop, list and set

--- python_test_conditional.py
+++ python_test_conditional.py
@@ -56,8 +56,66 @@ def fib(n):
 print(fib(2000))
 
 
+print("\n")
+print("\n")
+print("\n")
+
+a = 10/3
+b = 3.3333333
+
+if (a > b):
+	print("hahaha")
 
 
+
+print("\n")
+print("\n")
+print("\n")
+
+stack=[1,2,3]
+
+stack.append(5)
+
+print(stack)
+
+#stack.pop()
+
+print(stack.pop())
+print(stack.pop())
+print(stack)
+
+
+results = []
+for i in range(10):
+	results.append(i**2)
+
+print(results)
+
+results = list(map(lambda i:i**2, range(10)))
+
+#lambda is the function without a name, temporary function 
+#map is like apply function in R
+#same as:
+#def square(i):
+#return i**2
+print(results)
+
+results = [i**2 for i in range(10)]
+#short form of the last method
+print(results)
+
+
+fruit = {'apple','apple','banana','orange','pear','pinaapple','blueberry',
+'peach'}
+print(fruit)
+#difference between the array and the set
+#in the array, the sequence is remembered. while in the set, the sequence 
+#does not matter. and in the set, it only record the unique identification 
+#of the elements. even if you have two apples in the set, python will only 
+#print one. good property to use to get rid of repeated items
+
+tech = {'apple','blueberry'}
+print(fruit-tech)
 
 
 
